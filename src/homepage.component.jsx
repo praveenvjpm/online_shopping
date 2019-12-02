2aa352a351456637362c05a9648e0d857fbc10e4
praveenvjpm <praveenvjpm@gmail.com>
2019-12-02 18:11:41 +0530

    Project Initial setup

---
+++ src/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const homePage = () =>(
+    <div className='homepage'>
+        <div className="directoty-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1>Title</h1>
+                    <p>Shop</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
