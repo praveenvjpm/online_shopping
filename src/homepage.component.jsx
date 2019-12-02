da3dde2e6ea07159da8a91db9991f595d2b92afc
praveenvjpm <praveenvjpm@gmail.com>
2019-12-02 18:13:27 +0530

    Home page changes

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const homePage = () =>(
     <div className='homepage'>
-        <div className="directoty-menu">
+        <div className="directory-menu">
             <div className="menu-item">
                 <div className="content">
-                    <h1>Title</h1>
+                    <h1 className >Title</h1>
                     <p>Shop</p>
                 </div>
             </div>
