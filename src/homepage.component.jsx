c720c2625f676604e2fc0b3fd231fc925b9ff98c
praveenvjpm <praveenvjpm@gmail.com>
2019-12-03 21:58:15 +0530

    old files

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React from "react";
+import "./homepage.styles.scss";
 
-const homePage = () =>(
-    <div className='homepage'>
-        <div className="directory-menu">
-            <div className="menu-item">
-                <div className="content">
-                    <h1 className >Title</h1>
-                    <p>Shop</p>
-                </div>
-            </div>
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className>Title1</h1>
+          <p>Shop</p>
         </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className>Title2</h1>
+          <p>Shop</p>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className>Title3</h1>
+          <p>Shop</p>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className>Title4</h1>
+          <p>Shop</p>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+          <h1 className>Title5</h1>
+          <p>Shop</p>
+        </div>
+      </div>
     </div>
-)
+  </div>
+);
+
+export default HomePage;
