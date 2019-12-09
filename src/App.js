a60de91027235330a76f855883c989c47fdda4a1
praveenvjpm <praveenvjpm@gmail.com>
2019-12-09 22:24:09 +0530

    initialized module 6.2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 
 const HatsPage = () => {
   return (
@@ -16,7 +17,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop/hats" component={ShopPage} />
       </Switch>
     </div>
   );
