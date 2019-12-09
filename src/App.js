707648e1404ee5af1804a52ca03696f11cad8d34
praveenvjpm <praveenvjpm@gmail.com>
2019-12-10 00:26:27 +0530

    6.3 updated collection view list with css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import HomePage from "./pages/homepage/homepage.component";
 import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 
-const HatsPage = () => {
+/*const HatsPage = () => {
   return (
     <div>
       <h1>Hats Page</h1>
     </div>
   );
-};
+};*/
 
 function App() {
   return (
