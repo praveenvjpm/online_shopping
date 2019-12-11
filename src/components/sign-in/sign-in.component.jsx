3e22a959d0e735fb7acace09a8ce8195230aa611
praveenvjpm <praveenvjpm@gmail.com>
2019-12-11 23:15:23 +0530

    sign n component added

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./sign-in.styles.scss";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventdefault();
+    this.setState({ eamil: "", password: "" });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>Already Have an Account</h2>
+        <span>Sign in with your Email and Password</span>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          />
+          <label htmlFor="password">Password</label>
+
+          <input type="submit" value="submit form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
