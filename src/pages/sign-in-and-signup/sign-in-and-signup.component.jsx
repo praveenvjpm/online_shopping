5c51b73c237ce50d8cf5f6e50c4d17720df68969
Praveen Karanam Trinath -X (ptrinath - HCL TECHNOLOGIES LIMITED at Cisco) <ptrinath@partnet.cisco.com>
2019-12-11 19:10:31 +0530

    added files

---
+++ src/pages/sign-in-and-signup/sign-in-and-signup.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './sign-in-and-signup.styles.scss';
+
+
+const SignInAndSignUpPage  = () =>(
+    <div className="sign-in-and-sign-up">SigUp</div>
+);
+
+export default SignInAndSignUpPage;
