420bbbca8718862838eb338f919ff64b8d4fc235
praveenvjpm <praveenvjpm@gmail.com>
2019-12-29 23:20:29 +0530

    Design of signUp

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./custom-button.styles.scss";
-//Component//
+//Component
 const CustomButton = ({ children, ...OtherProps }) => (
   <button className="custom-button" {...OtherProps}>
     {children}
