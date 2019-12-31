d830cc494d70b7cd6e2c3608fc448c9934530491
praveenvjpm <praveenvjpm@gmail.com>
2019-12-31 22:58:50 +0530

    M6

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../forn-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
+//Component
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
